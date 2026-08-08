daa5504d279e47eb60c1e30fd7267e3c8ff54fc3
frantisekdron <frantisekdron@gmail.com>
2026-08-08 10:43:41 +0200

    Úprava kokpitu

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.CFG = {
   "branch": "main",
   "iterace": 600000,
   "blobs": {
-    "studio": null,
-    "klient": null
+    "studio": "TAjk5/o2iIX5CQLwfbCvDzj0RclNsGEuUJm0PFHelqgmwcaebKxa2P0PpidEVhoBUhQNGorlcIx49yU7DYvWiFUMyUHe0+75fqpjzj8Zt/kTqJfGXZGdN5Qb3XpAeHuH+mnRsdNJKvvJhfsZIgZHCXt0RGmQwQycN47gj5yxednupfPKAQr/I9c=",
+    "klient": "LmktNF+RzkZfIaFK7dy0JEOPPqXxGIwa5TGzAGskpyhHCCLXxOwk0r6iP1WFQt/G+HUVM2naKXTxqUWdoYuW2ZKiZeMne36fggagpyJD7FSCOGXmU4hrOHb/7Oh6pYWWaVINbZnZjfNuk2Kpoj7u5p/0oi6kiCpwlK60yh6O+oURZuSH8BVzf9s="
   }
 };
